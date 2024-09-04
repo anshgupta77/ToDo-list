5c3ffe9b4eb428eefc47057c060ca589955cb2bc
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2024-09-05 00:36:29 +0530

    shift up app.get

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const listRoutes = require("./routes/list");
 app.use(express.json());
 app.use(cors());
 
-app.use(morgan("dev"));
-app.use("/auth", authRoutes);
-app.use("/list", listRoutes);
-
 app.get("/", (req,res) =>{
     app.use(express.static(path.resolve(__dirname, "frontend copy", "build")));
     res.sendFile(path.resolve(__dirname, "frontend copy", "build", "index.html"));
 
 })
+app.use(morgan("dev"));
+app.use("/auth", authRoutes);
+app.use("/list", listRoutes);
+
 app.listen(PORT, () =>{
     console.log("Server is listen", PORT);
 })
